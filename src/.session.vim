c7f3ce5fe6d4aa3584e002f734fdcb0da1a3fb92
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-02 15:24:46 +0530

    Main Page Desktop

---
+++ src/.session.vim
@@ -0,0 +1,46 @@
+let SessionLoad = 1
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
+let v:this_session=expand("<sfile>:p")
+silent only
+silent tabonly
+cd ~/program/react/hackclubCucek/src
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+set shortmess=aoO
+badd +22 pages/index.js
+badd +67 components/seo.js
+badd +9 pages/page-2.js
+badd +5 styles/Colors.js
+badd +6 pages/404.js
+badd +36 components/Header.js
+badd +86 components/HeaderDesktop.js
+badd +4 styles/Global.css
+badd +8 components/Utility.js
+argglobal
+%argdel
+edit pages/index.js
+argglobal
+balt components/Utility.js
+let s:l = 22 - ((21 * winheight(0) + 22) / 44)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 22
+normal! 042|
+tabnext 1
+if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0&& getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20 shortmess=filnxtToOFc
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if filereadable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &g:so = s:so_save | let &g:siso = s:siso_save
+set hlsearch
+nohlsearch
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,99 @@
 import * as React from 'react';
-import {Link} from 'gatsby';
+import HeaderDesktop from '../components/HeaderDesktop';
+import {StyleSheet, css} from 'aphrodite';
+import background from '../images/bgDesktop.png';
+import '../styles/Global.css';
+import {getDeviceDimention} from '../components/Utility';
+import {COLORS} from '../styles/Colors';
 import {StaticImage} from 'gatsby-plugin-image';
 
-const IndexPage = () => (
-  <div>
-    GG
-    <div>hh</div>
-  </div>
-);
+const IndexPage = () => {
+  const width = getDeviceDimention().width;
+  if (width < 1280) {
+    return <div>Mobile view</div>;
+  }
 
+  // Desktop Page
+  return (
+    <div className={css(styles.rootDesktop)}>
+      <HeaderDesktop />
+
+      <div className={css(styles.container)}>
+        <p className={css(styles.title)}>Hack Club CUCEK</p>
+        <p className={css(styles.details)}>
+          A nonprofit network of high school coding clubs and makers around the
+          world, for the students, by the students.
+        </p>
+        <button className={css(styles.button)}>
+          Our Vision
+          <StaticImage src="../images/icons/nextIcon.png" alt="next-icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  rootDesktop: {
+    backgroundImage: `url(${background})`,
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: 250,
+    marginBottom: 'auto',
+    width: 'max-content',
+    height: 'max-content',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  title: {
+    fontFamily: 'Libre Franklin',
+    fontStyle: 'normal',
+    fontWeight: '900',
+    fontSize: '85.3333px',
+    lineHeight: '126.78%',
+    textAlign: 'center',
+    letterSpacing: '0.025em',
+    textTransform: 'uppercase',
+    textShadow: '3.08px solid #FFFFFF',
+    color: '#FFFFFF',
+    margin: 0,
+  },
+  details: {
+    fontFamily: 'Libre Franklin',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '24px',
+    lineHeight: '152%',
+    textAlign: 'center',
+    color: '#FFFFFF',
+    maxWidth: 840,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 4,
+  },
+  button: {
+    fontFamily: 'Libre Franklin',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: '26.6667px',
+    lineHeight: '128.7%',
+    letterSpacing: '0.0125em',
+    color: '#FFFFFF',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    background: COLORS.secondary,
+    borderRadius: '170.667px',
+    padding: 22,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: 270,
+    marginTop: 54,
+  },
+});
 export default IndexPage;
