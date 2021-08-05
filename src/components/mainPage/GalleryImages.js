200b46a32812f50dac1c90e1a8310077d72c3744
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-05 12:59:48 +0530

    Gallery images

--- src/components/mainPage/GalleryImages.js
+++ src/components/mainPage/GalleryImages.js
@@ -36,19 +36,31 @@ const GalleryImages = () => {
     },
   };
 
+  const get3Images = () => {
+    const offSet = imageIndex + 3;
+    if (offSet >= images.length) {
+      return [
+        ...images.slice(imageIndex),
+        ...images.slice(0, offSet - images.length),
+      ];
+    }
+
+    return images.slice(imageIndex, offSet);
+  };
+
   return (
     <div className={css(styles.root)}>
-      <AnimatePresence>
-        {images.map((item, id) => (
-          <motion.img
-            className={css(styles.images)}
-            key={id}
-            src={item}
-            variants={variants}
-            animate={id === imageIndex ? 'middle' : 'animate'}
-          />
-        ))}
-      </AnimatePresence>
+      {get3Images().map((item, id) => (
+        <motion.img
+          className={css(styles.images)}
+          key={id}
+          src={item}
+          animate={{
+            // x: -420 * imageIndex,
+            scale: id === 1 ? 1.2 : 1,
+          }}
+        />
+      ))}
     </div>
   );
 };
@@ -61,6 +73,7 @@ const styles = StyleSheet.create({
     overflowY: 'hidden',
     flex: 1,
     marginBottom: 100,
+    justifyContent: 'space-evenly',
   },
 
   images: {
