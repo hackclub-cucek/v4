02bdfad8016d4dae883032b711d36e89a6dfcbb9
Vyshnav-KS <Vyshnavks85@gmail.com>
2021-08-07 14:02:14 +0530

    bug

--- src/components/mainPageMobile/ThingsWeDoMob.js
+++ src/components/mainPageMobile/ThingsWeDoMob.js
@@ -2,11 +2,16 @@ import {css, StyleSheet} from 'aphrodite';
 import React from 'react';
 
 const ThingsWeDoMob = () => {
-  return <div className={css(styles.root)}>Starting soon </div>;
+  return (
+    <div className={css(styles.root)}>
+      <div className={css(styles.title)}>Things We Do</div>{' '}
+    </div>
+  );
 };
 const styles = StyleSheet.create({
   root: {
-    fontSize: 20,
+    display: 'flex',
+    flexDirection: 'column',
   },
 });
 export default ThingsWeDoMob;
