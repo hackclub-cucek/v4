f7d4aa48ce4970218d711d45cad4ad3e980d9860
Vyshnav-KS <Vyshnavks85@gmail.com>
2021-08-09 22:12:08 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ const IndexPage = ({data}) => {
           <HeaderMobile />
           <MainPageMobile />
         </BackgroundImage>
-        <ThingsWeDoMob />
+        <MainPageExtrasMob />
       </div>
     );
   }
